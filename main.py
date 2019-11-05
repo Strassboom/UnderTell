e02b5e9507ba601ea407c9b806e26b1866f2b938
Strassboom <maddog241996@gmail.com>
2019-11-05 01:24:45 -0500

    Cleaned up underteller.html. Implemented first fully working sans version for Windows 10.

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pydub import AudioSegment
-from pydub.silence import split_on_silence
+import simpleaudio as sa
+from pydub.effects import speedup
 
 app = Flask(__name__)
 
-@app.route('/')
-def home(text="You're gonna have a bad time"):
-    if len(text.strip()) == 0:
-        return render_template("underteller.html",message="")
-    
+@app.route('/',methods=["GET","POST"])
+def home():
+    if request.method == 'GET':
+        text = "Heya"
+    else:
+        text = request.form["words"]
     inSound = "static/voice_sans.wav"
     outSound = "static/result.wav"
 
-    # create 1 sec of silence audio segment
+    # create 0.1 sec of silence audio segment
     space_segment = AudioSegment.silent(duration=100)  #duration in milliseconds
 
     #read wav file to an audio segment
     song = AudioSegment.from_wav(inSound)
-    
-    #Add above two audio segments    
-    #final_song = one_sec_segment + song
-
-    #Either save modified audio
-    #final_song.export(audio_out_file, format="wav")
-
+    speedy_song = speedup(song * 6,10.0,crossfade=5)
     outVoice = AudioSegment.silent(duration=10)
 
     for character in text:
         if not character.isalnum():
-            outVoice = outVoice + space_segment
+            if character == " ":
+                outVoice = outVoice + song + space_segment
         else:
-            outVoice = outVoice + song
+            outVoice = outVoice + speedy_song
 
+    outVoice = speedup(outVoice,8.0)
     outVoice.export(outSound, format="wav")
 
-    aich = AudioSegment.from_wav(outSound)
-    #play(aich)
-    return render_template("underteller.html")
+    wave_obj = sa.WaveObject.from_wave_file("static/result.wav")
+    wave_obj.play()
+    return render_template("underteller.html",message=text)
 
 if __name__=="__main__":
-    app.run()
+    app.run(host="localhost", port=8080)
